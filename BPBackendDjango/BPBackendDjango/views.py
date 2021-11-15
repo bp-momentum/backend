9f7f8bd99966a962e657e2b968fb59225e69be9a
ErikPre <prescher-erik@web.de>
2021-11-15 21:50:55 +0100

    fixed Jannis typos

--- BPBackendDjango/BPBackendDjango/views.py
+++ BPBackendDjango/BPBackendDjango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.response import Rsponse
+from rest_framework.response import Response
 
 class TesView(APIView):
     def get(self, request, *args, **kwargs):
