cb3e22aa57f22dae365aa3383b3cf1e0051cbd37
Jannis25 <60541066+Jannis25@users.noreply.github.com>
2021-11-15 21:44:59 +0100

    typo fixed

--- BPBackendDjango/BPBackendDjango/serializers.py
+++ BPBackendDjango/BPBackendDjango/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-class UserSerializer(serialzers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
