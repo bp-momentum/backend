d8e176e06b50abbda98e8e4d7ad02a1e8c175c16
ErikPre <prescher-erik@web.de>
2021-11-19 16:58:28 +0100

    added Corsheaders middleware

--- BPBackendDjango/BPBackendDjango/settings.py
+++ BPBackendDjango/BPBackendDjango/settings.py
@@ -28,6 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = ['78.46.150.116']
 
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+    ]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'BPBackendDjango'
+    'BPBackendDjango',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
