334f489b630508ca654e7f0d10e58001932f2f81
Erik <prescher-erik@web.de>
2021-11-19 15:04:15 +0100

    add JWToken classe and implemented it in Register

---
+++ BPBackendDjango/BPBackendDjango/Helperclasses/jwttoken.py
@@ -0,0 +1,59 @@
+from jwcrypto import jwt, jwk
+import json
+from datetime import datetime, timedelta
+
+
+def check_tokentime(token_time):
+    now = datetime.now()
+    token_time = datetime.fromisoformat(token_time)
+    age = now - token_time
+    if timedelta(days=1) > age:
+        return True
+    return False
+
+    
+
+class JwToken(object):
+    
+    @staticmethod
+    def create_session_token(username):
+        #load key
+        key_dict = json.load(open("jw_key.json",))
+        key = jwk.JWK(**key_dict)
+
+        #sign token
+        signed_token = jwt.JWT(header={"alg": "HS256"}, claims={"username": username, "tokentime": str(datetime.now())})
+        signed_token.make_signed_token(key)
+
+        #encrypt the token
+        token = jwt.JWT(header={"alg": "A256KW", "enc": "A256CBC-HS512"}, claims=signed_token.serialize())
+        token.make_encrypted_token(key)
+        return token.serialize()
+
+    @staticmethod
+    def check_session_token(token):
+        #load key
+        key_dict = json.load(open("jw_key.json"))
+        key = jwk.JWK(**key_dict)
+        
+        #decrypt token
+        try:
+            ET = jwt.JWT(key=key, jwt = token)
+        except:
+            print("Decryption failed")
+            return False, ""
+        #check validation
+        try:
+            ST = jwt.JWT(key=key, jwt = ET.claims)
+        except:
+            print("Signature is not valid")
+            return False, ""
+        #check if the token is still valid (1 day)
+        info = json.loads(str(ST.claims))
+        
+        if not check_tokentime(info['tokentime']):
+            return False, info['username']
+
+        return True, info['username']
+
+    
--- BPBackendDjango/BPBackendDjango/Views/views.py
+++ BPBackendDjango/BPBackendDjango/Views/views.py
@@ -1,28 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from jwttoken import JwToken
+from ..Helperclasses.jwttoken import JwToken
 import hashlib
 
-from .serializers import *
-from .models import *
+from ..serializers import *
+from ..models import *
 
-class TesView(APIView):
-    def get(self, request, *args, **kwargs):
-        print("get get")
-        data = {
-            'irgendwas': 'schreiben',
-            'irgendwasanderes': 'manBinIchLustig'
-        }
-        return Response(data)
 
-    def post(self, request, *args, **kwargs):
-        print("post post")
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
 
 class RegisterView(APIView):
     def post(self, request, *args, **kwargs):
@@ -32,7 +17,7 @@ class RegisterView(APIView):
             #check if username already exists
             if not User.objects.filter(username=request.data['username']).exists():
                 #hashing password
-                serializer.fields['password']=hashlib.sha3_256(request.data['password']).hexdigest
+                serializer.fields['password'] = hashlib.sha3_256(request.data['password']).hexdigest
                 #save User in the databank
                 serializer.save()
                 #creating the session_token
