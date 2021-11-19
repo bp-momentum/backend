bb864fdd5e4013274fa93e5ec03cc1f28bfe7744
Erik <prescher-erik@web.de>
2021-11-19 15:33:39 +0100

    fixed serializer value change

--- BPBackendDjango/BPBackendDjango/Views/userviews.py
+++ BPBackendDjango/BPBackendDjango/Views/userviews.py
@@ -15,7 +15,7 @@ class RegisterView(APIView):
             #check if username already exists
             if not User.objects.filter(username=request.data['username']).exists():
                 #hashing password
-                serializer.fields['password'] = str(hashlib.sha3_256(request.data['password'].encode('utf8')).hexdigest())
+                serializer.data['password'] = str(hashlib.sha3_256(request.data['password'].encode('utf8')).hexdigest())
                 #save User in the databank
                 serializer.save()
                 #creating the session_token++
