cb5e47fb03e0dc798613cb9f38c91955b5af83fa
Erik <prescher-erik@web.de>
2021-11-19 15:14:51 +0100

    fixed tiny typo

--- BPBackendDjango/BPBackendDjango/urls.py
+++ BPBackendDjango/BPBackendDjango/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from Views.userviews import *
+from .Views.userviews import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
