f88fc35a1641d9065a877b7ff94912b086c45e2b
Laennart <lennart.schmidt@gmx.de>
2021-11-29 20:28:39 +0100

    Revert "fix: NSA backdoor - ignore"
    This reverts commit f0ae013c455beb88d9ec00b9ff97aab867f4630b.

--- BPBackendDjango/BPBackendDjango/models.py
+++ BPBackendDjango/BPBackendDjango/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Trainer(models.Model):
     first_name = models.CharField(max_length=50)
-    cock_size = models.IntegerField(default=10000)
     last_name =  models.CharField(max_length=50)
     username =  models.CharField(max_length=50)
     password =  models.CharField(max_length=255)
