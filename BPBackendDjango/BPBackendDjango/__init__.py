60011ef7c45c32ad056a905cee581c0cc47fcfb0
Laennart <lennart.schmidt@gmx.de>
2021-11-29 22:04:41 +0100

    refactor: remove prints

--- BPBackendDjango/BPBackendDjango/__init__.py
+++ BPBackendDjango/BPBackendDjango/__init__.py
@@ -8,15 +8,11 @@ import hashlib
 from .models import Admin
 from .settings import INTERN_SETTINGS
 
-## check if at least one admin account exists
-print("checking admin")
+# check if at least one admin account exists
 try:
     if not Admin.objects.filter().exists():
-        print("no admin found")
-        newAdmin = Admin(first_name="Admin", last_name="Admin", username=INTERN_SETTINGS["admin_username"], password= str(hashlib.sha3_256(INTERN_SETTINGS["admin_password"].encode('utf-8')).hexdigest()))
+        newAdmin = Admin(first_name="Admin", last_name="Admin", username=INTERN_SETTINGS["admin_username"],
+                         password=str(hashlib.sha3_256(INTERN_SETTINGS["admin_password"].encode('utf-8')).hexdigest()))
         newAdmin.save()
-        print("admin is now created")
-    else:
-        print("admin found")
 except:
-    print("Error!!! ALARM!")
+    pass
