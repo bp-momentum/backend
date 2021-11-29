966edcf1d244d7c041d321dedcc908cf556de3cc
Erik <prescher-erik@web.de>
2021-11-29 16:41:59 +0100

    changed test

--- BPBackendDjango/BPBackendDjango/testing.py
+++ BPBackendDjango/BPBackendDjango/testing.py
@@ -4,7 +4,7 @@ from .models import *
 class UserTestCase(TestCase):
     def setUp(self):
         Trainer.objects.create(first_name="Erik", last_name="Prescher", username="DerTrainer", email_address="prescher-erik@web.de", password="Password1234")
-        trainer = Trainer.objects.get(id=1)
+        trainer = Trainer.objects.get(first_name="Erik")
         User.objects.create(first_name="Erik", last_name="Prescher", username="DeadlyFarts", trainer=trainer, email_address="prescher-erik@web.de", password="Password1234")
     
     def test_if_exists(self):
@@ -12,6 +12,6 @@ class UserTestCase(TestCase):
         self.assertTrue(User.objects.filter(first_name="Erik", last_name="Prescher", username="DeadlyFarts", trainer=1, email_address="prescher-erik@web.de", password="Password1234").exists())
 
     def test_if_user_gets_deleted_when_trainer_gets_deleted(self):
-        Trainer.objects.delete(first_name="Erik", last_name="Prescher", username="DerTrainer", email_address="prescher-erik@web.de", password="Password1234")
+        Trainer.objects.filter(first_name="Erik", last_name="Prescher", username="DerTrainer", email_address="prescher-erik@web.de", password="Password1234").delete()
         self.assertFalse(Trainer.objects.filter(first_name="Erik", last_name="Prescher", username="DerTrainer", email_address="prescher-erik@web.de", password="Password1234").exists())
         self.assertFalse(User.objects.filter(first_name="Erik", last_name="Prescher", username="DeadlyFarts", trainer=1, email_address="prescher-erik@web.de", password="Password1234").exists())
