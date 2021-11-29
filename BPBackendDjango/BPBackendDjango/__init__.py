3de944be2d9b37b27c19a7e7b4289c11b0871a36
Erik <prescher-erik@web.de>
2021-11-29 17:51:56 +0100

    added prints

--- BPBackendDjango/BPBackendDjango/__init__.py
+++ BPBackendDjango/BPBackendDjango/__init__.py
@@ -5,5 +5,10 @@ from .models import Admin
 from .settings import INTERN_SETTINGS
 
 ## check if at least one admin account exists
+print("checking admin")
 if not Admin.objects.filter().exists():
+    print("now admin found")
     newAdmin = Admin(first_name="Admin", last_name="Admin", username=INTERN_SETTINGS["admin_username"], password= str(hashlib.sha3_256(INTERN_SETTINGS["admin_password"].encode('utf-8')).hexdigest()))
+    print("admin is now created")
+else:
+    print("admin found")
