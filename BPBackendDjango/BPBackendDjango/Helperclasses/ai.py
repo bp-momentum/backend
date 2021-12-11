5fac452715840278226a5402f97643225605f797
Jannis25 <jannislb@gmail.com>
2021-12-11 12:17:34 +0100

    feat: dummy result tupel of intensity, speed and cleanliness

--- BPBackendDjango/BPBackendDjango/Helperclasses/ai.py
+++ BPBackendDjango/BPBackendDjango/Helperclasses/ai.py
@@ -3,13 +3,14 @@ import math
 
 class AIInterface():
     def call_ai(exercise, video, username):
+        #TODO assign result user
         return DummyAI.dummy_function(exercise, video)
 
 
 class DummyAI():
     def dummy_function(ex, video):
         return {
-            'performance': math.ceil(random.random()*100+900),
-            'speed': math.ceil(random.random()*100+900),
-            'test': math.ceil(random.random()*100+900)
+            'intensity': math.ceil(random.random()*10000)/100,
+            'speed': math.ceil(random.random()*10000)/100,
+            'cleanliness': math.ceil(random.random()*10000)/100
         }
