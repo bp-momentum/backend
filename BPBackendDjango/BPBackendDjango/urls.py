9e6c1bb3ddd020bdde07071148525737bc26d5f1
Erik Prescher <prescher-erik@web.de>
2021-12-20 00:47:43 +0100

    mad dumb f..ing typo

--- BPBackendDjango/BPBackendDjango/urls.py
+++ BPBackendDjango/BPBackendDjango/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
     path('api/deleteplan', DeletePlanView.as_view(), name='deletePlan'),
     #leaderboard
     path('api/listleaderboard', ListLeaderboard.as_view(), name='listLeaderboard'),
-    path('api/addentry', AddEntry.as_view(), name='addEntry')g
+    path('api/addentry', AddEntry.as_view(), name='addEntry')
 ]
