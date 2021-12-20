27f2092e0d18ee2bd9393c179a3c6ef22403d4bf
Erik Prescher <prescher-erik@web.de>
2021-12-20 01:12:23 +0100

    i hate myself

--- BPBackendDjango/BPBackendDjango/Views/leaderboardviews.py
+++ BPBackendDjango/BPBackendDjango/Views/leaderboardviews.py
@@ -8,8 +8,8 @@ from ..Helperclasses.jwttoken import JwToken
 class AddEntry(APIView):
     def post(self, request, *args, **kwargs):
         req_data = dict(request.data)
-        user = User.objects.filter(id=req_data['user'])
-        entry = Leaderboard(user=user.id, score=req_data['score'])
+        user = User.objects.filter(username=req_data['user'])
+        entry = Leaderboard(user=user, score=req_data['score'])
         entry.save()
         print("saved")
         data = {
