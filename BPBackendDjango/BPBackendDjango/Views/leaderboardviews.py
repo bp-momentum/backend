a7450a5333fe61b3f7a5ec3c47b9105edbe02b53
Erik Prescher <prescher-erik@web.de>
2021-12-20 18:17:39 +0100

    now possible to request specific count of Leaderboard entries

--- BPBackendDjango/BPBackendDjango/Views/leaderboardviews.py
+++ BPBackendDjango/BPBackendDjango/Views/leaderboardviews.py
@@ -1,3 +1,5 @@
+import math
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,7 +8,7 @@ from ..Helperclasses.jwttoken import JwToken
 
 
 class ListLeaderboard(APIView):
-    def get(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         req_data = dict(request.data)
         v, token = JwToken.check_session_token(request.headers['Session-Token'])
         if not v:
@@ -19,9 +21,41 @@ class ListLeaderboard(APIView):
 
         leaderboard = Leaderboard.objects.order_by("-score")
         out = []
-        for l in leaderboard:
-            entry = {"username": l.user.username, "score": l.score}
-            out.append(entry)
+        rank = 0
+        count_of_entries = req_data['count']
+
+        # they are just as many entries as requested
+        if len(leaderboard) <= count_of_entries:
+            for l in leaderboard:
+                rank += 1
+                entry = {"rank": rank, "username": l.user.username, "score": l.score}
+                out.append(entry)
+
+        user_index = leaderboard.index(Leaderboard.objects.get(user=User.objects.get(username=token["username"])))
+        count_entries = len(leaderboard)
+        # user is in top count_of_series
+        if user_index < math.floor(count_of_entries/2):
+            for l in range(0, count_of_entries):
+                rank += 1
+                entry = {"rank": rank, "username": leaderboard[l].user.username, "score": leaderboard[l].score}
+                out.append(entry)
+
+
+        # user in bottom count_of_series
+        elif user_index > count_entries - math.ceil(count_of_entries/2):
+            rank = count_entries - count_of_entries
+            for l in range(count_entries - count_of_entries, count_entries):
+                rank += 1
+                entry = {"rank": rank, "username": leaderboard[l].user.username, "score": leaderboard[l].score}
+                out.append(entry)
+
+        else:
+            for l in range(user_index - math.floor(count_of_entries/2), user_index + math.ceil(count_entries/2)):
+                rank += 1
+                entry = {"rank": rank, "username": leaderboard[l].user.username, "score": leaderboard[l].score}
+                out.append(entry)
+
+
 
         data = {
             'success': True,
