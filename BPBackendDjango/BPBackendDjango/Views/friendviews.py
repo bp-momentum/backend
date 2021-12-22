c680aefed52453d369d5b9e3192a74237160d438
Jannis25 <jannislb@gmail.com>
2021-12-22 10:25:32 +0100

    wip: delete friend view

--- BPBackendDjango/BPBackendDjango/Views/friendviews.py
+++ BPBackendDjango/BPBackendDjango/Views/friendviews.py
@@ -299,3 +299,26 @@ class DeclineRequestView(APIView):
 
 
 class DeleteFriendView(APIView):
+    def post(self, request, *args, **kwargs):
+        token = JwToken.check_session_token(request.headers['Session-Token'])
+        #check if token is valid
+        if not token["valid"]:
+            data = {
+                'success': False,
+                'description': 'Token is not valid',
+                'data': {}
+                }
+            return Response(data)
+
+        info = token['info']
+
+        #must be user
+        if not User.objects.filter(username=info['username']).exists():
+            data = {
+                    'success': False,
+                    'description': 'Not a user',
+                    'data': {}
+                }
+            return Response(data)
+
+        user = User.objects.get(username=info['username'])
