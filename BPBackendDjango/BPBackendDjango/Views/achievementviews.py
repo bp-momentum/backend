24cadc7d4d5ed9ccb4dbb123d3bbe5f84ec85a5b
Jannis25 <jannislb@gmail.com>
2021-12-28 11:00:18 +0100

    wip: achievements doneExcersis and havingFriends implemented

--- BPBackendDjango/BPBackendDjango/Views/achievementviews.py
+++ BPBackendDjango/BPBackendDjango/Views/achievementviews.py
@@ -68,14 +68,64 @@ class GetAchievementsView(APIView):
 
         #iterate over all existing achievements
         for achievement in Achievment.objects.all():
-            if achievement.name == '':
-                if True: #überprüfen der Bedingen zum erreichen
+            #do excersises
+            if achievement.name == 'doneExercises':
+                nr_of_exs = len(DoneExercises.objects.filter(user=user.id))
+                if nr_of_exs >= 100:
+                    upgrade_level(user.id, 'doneExercises', 3)
                     achieved.append({
                         'name': achievement.name,
                         'description': achievement.description,
+                        'level': 3,
                         'progress': 'done',
                         'hidden': achievement.hidden
-                    }) #hinzufügen zur Liste
+                    }) 
+                elif nr_of_exs >= 50:
+                    upgrade_level(user.id, 'doneExercises', 2)
+                    achieved.append({
+                        'name': achievement.name,
+                        'description': achievement.description,
+                        'level': 2,
+                        'progress': str(nr_of_exs)+'/100',
+                        'hidden': achievement.hidden
+                    }) 
+                elif nr_of_exs >= 10:
+                    achieve_achievement(user.id, 'doneExercises')
+                    achieved.append({
+                        'name': achievement.name,
+                        'description': achievement.description,
+                        'level': 1,
+                        'progress': str(nr_of_exs)+'/50',
+                        'hidden': achievement.hidden
+                    }) 
+                elif not achievement.hidden:
+                    achieved.append({
+                        'name': achievement.name,
+                        'description': achievement.description,
+                        'level': 0,
+                        'progress': str(nr_of_exs)+'/10',
+                        'hidden': achievement.hidden
+                    })
+            #make a friend
+            elif achievement.name == 'havingFriends':
+                nr_of_friends = len(Friends.objects.filter(friend1=user.id).union(Friends.objects.filter(friend2=user.id)))
+                if nr_of_friends >= 1:
+                    achieve_achievement(user.id, 'havingFriends')
+                    achieved.append({
+                        'name': achievement.name,
+                        'description': achievement.description,
+                        'level': 1,
+                        'progress': 'done',
+                        'hidden': achievement.hidden
+                    }) 
+                elif not achievement.hidden:
+                    achieved.append({
+                        'name': achievement.name,
+                        'description': achievement.description,
+                        'level': 0,
+                        'progress': '0/1',
+                        'hidden': achievement.hidden
+                    }) 
 
 
         data = {
