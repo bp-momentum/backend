8f67a7daf67b42a8738d6b2a714ec1d33a092fc2
Erik Prescher <prescher-erik@web.de>
2022-01-01 16:09:36 +0100

    added automical generate standard user and trainer

--- BPBackendDjango/BPBackendDjango/__init__.py
+++ BPBackendDjango/BPBackendDjango/__init__.py
@@ -5,7 +5,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BPBackendDjango.settings')
 django.setup()
 import hashlib
-from .models import Admin
+from .models import Admin, User, Trainer
 from .settings import INTERN_SETTINGS
 
 # check if at least one admin account exists
@@ -14,5 +14,24 @@ try:
         newAdmin = Admin(first_name="Admin", last_name="Admin", username=INTERN_SETTINGS["admin_username"],
                          password=str(hashlib.sha3_256(INTERN_SETTINGS["admin_password"].encode('utf-8')).hexdigest()))
         newAdmin.save()
+except:
+    pass
+
+try:
+    if not User.objects.filter().exists():
+        newUser = User(first_name="User", last_name="User", username=INTERN_SETTINGS["user_username"],
+                       password=str(hashlib.sha3_256(INTERN_SETTINGS["user_password"].encode('utf-8')).hexdigest()))
+
+        newUser.save()
+except:
+    pass
+
+try:
+    if not Trainer.objects.filter().exists():
+        newTrainer = Trainer(first_name="User", last_name="User", username=INTERN_SETTINGS["trainer_username"],
+                             password=str(
+                                 hashlib.sha3_256(INTERN_SETTINGS["trainer_password"].encode('utf-8')).hexdigest()))
+
+        newTrainer.save()
 except:
     pass
