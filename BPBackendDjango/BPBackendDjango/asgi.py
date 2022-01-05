102046276f6595e32269c6806ab1c594aa51ec73
Erik Prescher <prescher-erik@web.de>
2022-01-05 14:53:39 +0100

    changed websocket url

--- BPBackendDjango/BPBackendDjango/asgi.py
+++ BPBackendDjango/BPBackendDjango/asgi.py
@@ -3,7 +3,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from BPBackendDjango.BPBackendDjango import routing
+from BPBackendDjango.BPBackendDjango.routing import websocket_urlpatterns
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
@@ -11,7 +11,7 @@ application = ProtocolTypeRouter({
   "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
-            routing.websocket_urlpatterns
+            websocket_urlpatterns
         )
     ),
 })
