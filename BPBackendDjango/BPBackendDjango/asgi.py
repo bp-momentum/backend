e05ed228009099249d14486c86809e89169c8b1a
Erik Prescher <prescher-erik@web.de>
2022-01-05 15:03:56 +0100

    next try

--- BPBackendDjango/BPBackendDjango/asgi.py
+++ BPBackendDjango/BPBackendDjango/asgi.py
@@ -3,9 +3,9 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from BPBackendDjango.BPBackendDjango.routing import websocket_urlpatterns
+from .routing import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BPBackendDjango.settings")
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
