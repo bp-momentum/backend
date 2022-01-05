ea9b8c1a92d7467df2b6a1cd203c718ebeec962d
Erik Prescher <prescher-erik@web.de>
2022-01-05 15:34:10 +0100

    path to re_path

--- BPBackendDjango/BPBackendDjango/routing.py
+++ BPBackendDjango/BPBackendDjango/routing.py
@@ -1,7 +1,7 @@
-from django.urls import path
+from django.urls import re_path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    path('ws/socket', consumers.ChatConsumer.as_asgi()),
+    re_path('ws/socket', consumers.ChatConsumer.as_asgi()),
 ]
