301c7553b97771736f118f7d672744ab2189fe8e
Erik Prescher <prescher-erik@web.de>
2022-01-06 17:16:40 +0100

    added start/end set and video stream

--- BPBackendDjango/BPBackendDjango/consumers.py
+++ BPBackendDjango/BPBackendDjango/consumers.py
@@ -1,9 +1,33 @@
 from channels.generic.websocket import WebsocketConsumer
 
 import json
-from channels.generic.websocket import WebsocketConsumer
+import asyncio
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
+from BPBackendDjango.BPBackendDjango.Helperclasses.ai import AIInterface, DummyAI
+
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    def __init__(self):
+        self.doing_set = False
+
+    async def send_stats(self, ex_id):
+        while self.doing_set:
+            # calculating points
+            a, b, c = DummyAI.dummy_function(ex=ex_id, video=None)
+            intensity = b['intensity']
+            speed = b['speed']
+            cleanliness = b['cleanliness']
+            await self.send(text_data=json.dumps({
+                'success': True,
+                'description': "These are the current stats of this set",
+                'data': {
+                    'intensity': intensity,
+                    'speed': speed,
+                    'cleanliness': cleanliness
+                }
+            }))
+
     def connect(self):
         self.accept()
 
@@ -12,8 +36,25 @@ class ChatConsumer(WebsocketConsumer):
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        m_type = text_data_json['message_type']
+        data = text_data_json['data']
+
+        if m_type == "video_stream":
+
+            exercise = data['exercise']
+            video = data['video']
+
+            if not self.doing_set:
+                self.send(text_data=json.dumps({
+                    'success': False,
+                    'description': "The set must be started to send the video Stream",
+                    'data': {}
+                }))
+            AIInterface.call_ai(exercise, video)
+
+        elif m_type == "start_set":
+            self.doing_set = True
+
+        elif m_type == "end_set":
+            self.doing_set = False
