8166e8c4d34babf2c14f24cebdbb58f4d4b4f253
Jannis25 <jannislb@gmail.com>
2022-01-15 12:13:44 +0100

    wip: change contact data

--- BPBackendDjango/BPBackendDjango/Views/userviews.py
+++ BPBackendDjango/BPBackendDjango/Views/userviews.py
@@ -988,7 +988,7 @@ class SetTrainerLocationView(APIView):
                 'data': {}
             }
             return Response(data)
-            
+
         trainer = Trainer.objects.get(username=info['username'])
         if not Location.objects.filter(street=req_data['street'], postal_code=req_data['postal_code'], country=req_data['country'], city=req_data['city'], house_nr=req_data['house_nr'], address_addition=req_data['address_add']).exists():
             loc = Location.objects.create(street=req_data['street'], postal_code=req_data['postal_code'], country=req_data['country'], city=req_data['city'], house_nr=req_data['house_nr'], address_addition=req_data['address_add'])
@@ -999,6 +999,41 @@ class SetTrainerLocationView(APIView):
         data = {
             'success': True,
             'description': 'Location updated',
+            'data': {}
+        }
+        return Response(data)
+
+
+class ChangeTelephoneView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        req_data = request.data
+        token = JwToken.check_session_token(request.headers['Session-Token'])
+        #check if token is valid
+        if not token["valid"]:
+            data = {
+                    'success': False,
+                    'description': 'Token is not valid',
+                    'data': {}
+                }
+            return Response(data)
+        info = token['info']
+
+        #check if requested by trainer
+        if not info['account_type'] == 'trainer':
+            data = {
+                'success': False,
+                'description': 'Not a trainer',
+                'data': {}
+            }
+            return Response(data)
+            
+        trainer = Trainer.objects.get(username=info['username'])
+        trainer.telephone = req_data['telephone']
+        trainer.save()
+        data = {
+            'success': True,
+            'description': 'Telephone number updated',
             'data': {}
         }
         return Response(data)
