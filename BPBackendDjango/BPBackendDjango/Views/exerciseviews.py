3d577d9a31160c120018f90e1a9b4cded06f2af8
Jannis25 <jannislb@gmail.com>
2022-01-31 16:20:46 +0100

    fix: weird

--- BPBackendDjango/BPBackendDjango/Views/exerciseviews.py
+++ BPBackendDjango/BPBackendDjango/Views/exerciseviews.py
@@ -54,8 +54,7 @@ def get_done_exercises_of_month(month, year, user):
         year_offset = (year-1970)*SECS_PER_YEAR
         month_offset = 0
         for i in range(month-1):
-            if i != 0:
-                month_offset += get_lastday_of_month(i, year)*SECS_PER_DAY
+            month_offset += get_lastday_of_month(i, year)*SECS_PER_DAY
         next_month_offset = month_offset + get_lastday_of_month(month, year) * SECS_PER_DAY
         offset_gt = year_offset + month_offset - 3600
         offset_lt = year_offset + next_month_offset - 3600
