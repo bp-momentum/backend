25ed500c1fdca19c4909920e56d3a5f62c8cfb34
Erik <prescher-erik@web.de>
2022-03-10 16:09:11 +0100

    fix: changed calculation of score for leaderboard entry

--- BPBackendDjango/BPBackendDjango/testing.py
+++ BPBackendDjango/BPBackendDjango/testing.py
@@ -1496,12 +1496,16 @@ class TestLeaderboardView(TestCase):
         self.assertTrue(response.data.get('success'))
         leaderboard = []
         entry = Leaderboard.objects.get(score=90)
-        leaderboard.append({"rank": 2, "username": entry.user.username, "score": 90.0, "cleanliness": 90.0, "intensity": 90.0, "speed": 90.0})
+        leaderboard.append({"rank": 2, "username": entry.user.username, "score": 90})
         entry = Leaderboard.objects.get(score=80)
-        leaderboard.append({"rank": 3, "username": entry.user.username, "score": 80.0, "cleanliness": 80.0, "intensity": 80.0, "speed": 80.0})
+        leaderboard.append({"rank": 3, "username": entry.user.username, "score": 80})
         entry = Leaderboard.objects.get(score=70)
-        leaderboard.append({"rank": 4, "username": entry.user.username, "score": 70.0, "cleanliness": 70.0, "intensity": 70.0, "speed": 70.0})
-        self.assertEquals(response.data.get('data').get('leaderboard'), leaderboard)
+        leaderboard.append({"rank": 4, "username": entry.user.username, "score": 70})
+        get_response = []
+        for ent in response.data.get('data').get('leaderboard'):
+            get_response.append({"rank": ent.get('rank'), "username": ent.get('username'), "score": ent.get('score')})
+
+        self.assertEquals(get_response, leaderboard)
         #invalid
         #invalid token
         request = ViewSupport.setup_request({'Session-Token': 'invalid'}, {'count': 3})
