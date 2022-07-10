880754cf928063f1ea314e69d881640e5cc3c49a
Julian Imhof <julianimhof00@gmail.com>
2022-07-11 00:16:52 +0200

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,8 @@ python3 manage.py makemigrations MomentumBackend
 
 python3 manage.py migrate MomentumBackend --fake-initial
 
-if [[ -z "${MOMENTUM_TEST}" ]]; then
+if [ -z "$MOMENTUM_TEST" ]; then
   python3 manage.py runserver 0.0.0.0:8000
 else
   python3 manage.py test
 fi
-
