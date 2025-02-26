7e20fadf767299f0a64f638b0795ed6fbcabcd07
dill-kismed <dill@kismed.tu-darmstadt.de>
2025-02-26 14:12:17 +0100

    reordering of middlewares
    fix CORS

--- MomentumBackend/settings.py
+++ MomentumBackend/settings.py
@@ -80,9 +80,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
